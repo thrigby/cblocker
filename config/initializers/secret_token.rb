40e662404f9db948f0d5a6537dcac40b6565e21a
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-05 17:20:59 +0800

    Delete config directory

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Cblocker::Application.config.secret_token = '56f1330e8acfedf77a6c62327be0573bb67089426369f21a7f3aab1d8e11c287b7ec4ec1fc6426dffaef54aacbf046b966050e7c849c51c2a5a91a5e6d0ed7c7'
